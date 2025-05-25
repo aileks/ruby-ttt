53613c00f37246b4eda6ac40a2c1d0cb89efd0ef
Aaliyah Harper <aeverly14@pm.me>
2025-05-25 16:49:15 -0400

    add gitignore

--- lib/tic_tac_toe/board.rb
+++ lib/tic_tac_toe/board.rb
@@ -1,1 +1,14 @@
-# TODO
+class Board
+  def initialize
+    @board = Array.new(3) { Array.new(3, nil) }
+  end
+
+  def display
+    puts('Current Board:')
+    @board.each do |row|
+      row_display = row.map { |cell| cell.nil? ? '_' : cell }.join(' | ')
+      puts(row_display)
+      puts('-' * 9)
+    end
+  end
+end
